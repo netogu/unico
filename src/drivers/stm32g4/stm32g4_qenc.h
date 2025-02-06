facc42564dc542cb3e6e7dad0ccb5d7e55f9e296
netogu <ernesto.gonzalez.u@gmail.com>
2025-02-05 19:31:47 -0800

    adding q31 support to qenc

--- src/drivers/stm32g4/stm32g4_qenc.h
+++ src/drivers/stm32g4/stm32g4_qenc.h
@@ -18,7 +18,7 @@ void qenc_run(qenc_t *self);
 void qenc_stop(qenc_t *self);
 void qenc_reset(qenc_t *self);
 void qenc_load(qenc_t *self, uint32_t count);
-uint32_t qenc_read(qenc_t *self);
+int32_t qenc_read(qenc_t *self);
 int qenc_timer_set_frequency(qenc_t *self, uint32_t hz);
 
 #endif // stm32g4_encoder.h
